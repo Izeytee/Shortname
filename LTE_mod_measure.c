d372f75b373d629415d3998660c59e35c5052e09
Izeytee <I-zey@yandex.ru>
2026-06-20 19:18:30 +0700

    changed m2 to m1

--- LTE_mod_measure.c
+++ LTE_mod_measure.c
@@ -256,38 +256,38 @@ void optQPSK(const uint8_t *pSrc, T16sc *pDst, uint32_t length)
 
 void optQAM16(const uint8_t *pSrc, T16sc *pDst, uint32_t length)
 {
-    size_t table_vl =  __riscv_vsetvl_e8m2(sizeof(mod16QAM_LUT) / sizeof(*mod16QAM_LUT));
+    size_t table_vl =  __riscv_vsetvl_e8m1(sizeof(mod16QAM_LUT) / sizeof(*mod16QAM_LUT));
 
-    vint8m2_t table = __riscv_vle8_v_i8m2(mod16QAM_LUT, table_vl);
+    vint8m1_t table = __riscv_vle8_v_i8m1(mod16QAM_LUT, table_vl);
 
     for (uint32_t i = 0; i < length;)
     {
-        size_t vl =  __riscv_vsetvl_e8m2(length - i);
+        size_t vl =  __riscv_vsetvl_e8m1(length - i);
 
-        vuint8m2_t src = __riscv_vle8_v_u8m2(pSrc + i, vl);
+        vuint8m1_t src = __riscv_vle8_v_u8m1(pSrc + i, vl);
 
-        vuint8m2_t b2 = __riscv_vand_vx_u8m2(src, 0x4, vl);
-        vuint8m2_t b1 = __riscv_vand_vx_u8m2(src, 0x2, vl);
+        vuint8m1_t b2 = __riscv_vand_vx_u8m1(src, 0x4, vl);
+        vuint8m1_t b1 = __riscv_vand_vx_u8m1(src, 0x2, vl);
         
-        vuint8m2_t swapped = __riscv_vor_vv_u8m2(
-            __riscv_vsrl_vx_u8m2(b2, 1, vl),
-            __riscv_vsll_vx_u8m2(b1, 1, vl), vl);
+        vuint8m1_t swapped = __riscv_vor_vv_u8m1(
+            __riscv_vsrl_vx_u8m1(b2, 1, vl),
+            __riscv_vsll_vx_u8m1(b1, 1, vl), vl);
 
-        vuint8m2_t grouped = __riscv_vand_vx_u8m2(src, 0x9, vl);
-        grouped = __riscv_vor_vv_u8m2(grouped, swapped, vl);
+        vuint8m1_t grouped = __riscv_vand_vx_u8m1(src, 0x9, vl);
+        grouped = __riscv_vor_vv_u8m1(grouped, swapped, vl);
 
-        vuint8m2_t srcRe = __riscv_vsrl_vx_u8m2(__riscv_vand_vx_u8m2(grouped, 0xC, vl), 0x2, vl);
-	    vuint8m2_t srcIm = __riscv_vand_vx_u8m2(grouped, 0x3, vl);
+        vuint8m1_t srcRe = __riscv_vsrl_vx_u8m1(__riscv_vand_vx_u8m1(grouped, 0xC, vl), 0x2, vl);
+	    vuint8m1_t srcIm = __riscv_vand_vx_u8m1(grouped, 0x3, vl);
 
-        vint8m2_t outRe = __riscv_vrgather_vv_i8m2(table, srcRe, vl);
-        vint8m2_t outIm = __riscv_vrgather_vv_i8m2(table, srcIm, vl);
+        vint8m1_t outRe = __riscv_vrgather_vv_i8m1(table, srcRe, vl);
+        vint8m1_t outIm = __riscv_vrgather_vv_i8m1(table, srcIm, vl);
 
-        vint16m4_t outWRe = __riscv_vmul_vx_i16m4(__riscv_vwcvt_x_x_v_i16m4(outRe, vl), C_16QAM, vl);
-        vint16m4_t outWIm = __riscv_vmul_vx_i16m4(__riscv_vwcvt_x_x_v_i16m4(outIm, vl), C_16QAM, vl);
+        vint16m2_t outWRe = __riscv_vmul_vx_i16m2(__riscv_vwcvt_x_x_v_i16m2(outRe, vl), C_16QAM, vl);
+        vint16m2_t outWIm = __riscv_vmul_vx_i16m2(__riscv_vwcvt_x_x_v_i16m2(outIm, vl), C_16QAM, vl);
 
-	    vint16m4x2_t outSeg = __riscv_vcreate_v_i16m4x2(outWRe, outWIm);
+	    vint16m2x2_t outSeg = __riscv_vcreate_v_i16m2x2(outWRe, outWIm);
 
-        __riscv_vsseg2e16_v_i16m4x2((int16_t*)pDst + i * 2, outSeg, vl);
+        __riscv_vsseg2e16_v_i16m2x2((int16_t*)pDst + i * 2, outSeg, vl);
 
         i += vl;
     }
@@ -295,30 +295,30 @@ void optQAM16(const uint8_t *pSrc, T16sc *pDst, uint32_t length)
 
 void optQAM16_proxy(const uint8_t *pSrc, T16sc *pDst, uint32_t length)
 {
-    size_t table_vl =  __riscv_vsetvl_e8m2(sizeof(mod16QAM_LUT) / sizeof(*mod16QAM_LUT));
+    size_t table_vl =  __riscv_vsetvl_e8m1(sizeof(mod16QAM_LUT) / sizeof(*mod16QAM_LUT));
 
-    vint8m2_t table = __riscv_vle8_v_i8m2(mod16QAM_LUT, table_vl);
+    vint8m2_t table = __riscv_vle8_v_i8m1(mod16QAM_LUT, table_vl);
 
     for (uint32_t i = 0; i < length;)
     {
-        size_t vl =  __riscv_vsetvl_e8m2(length - i);
+        size_t vl =  __riscv_vsetvl_e8m1(length - i);
 
-        vuint8m2_t src = __riscv_vle8_v_u8m2(pSrc + i, vl);
+        vuint8m1_t src = __riscv_vle8_v_u8m1(pSrc + i, vl);
 
-        vuint8m2_t grouped = __riscv_vand_vx_u8m2(src, 0x9, vl); // assume it is vector unzip
+        vuint8m1_t grouped = __riscv_vand_vx_u8m1(src, 0x9, vl); // assume it is vector unzip
 
-        vuint8m2_t srcRe = __riscv_vsrl_vx_u8m2(__riscv_vand_vx_u8m2(grouped, 0xC, vl), 0x2, vl);
-        vuint8m2_t srcIm = __riscv_vand_vx_u8m2(grouped, 0x3, vl);
+        vuint8m1_t srcRe = __riscv_vsrl_vx_u8m1(__riscv_vand_vx_u8m1(grouped, 0xC, vl), 0x2, vl);
+        vuint8m1_t srcIm = __riscv_vand_vx_u8m1(grouped, 0x3, vl);
 
-        vint8m2_t outRe = __riscv_vrgather_vv_i8m2(table, srcRe, vl);
-        vint8m2_t outIm = __riscv_vrgather_vv_i8m2(table, srcIm, vl);
+        vint8m1_t outRe = __riscv_vrgather_vv_i8m1(table, srcRe, vl);
+        vint8m1_t outIm = __riscv_vrgather_vv_i8m1(table, srcIm, vl);
 
-        vint16m4_t outWRe = __riscv_vmul_vx_i16m4(__riscv_vwcvt_x_x_v_i16m4(outRe, vl), C_16QAM, vl);
-        vint16m4_t outWIm = __riscv_vmul_vx_i16m4(__riscv_vwcvt_x_x_v_i16m4(outIm, vl), C_16QAM, vl);
+        vint16m2_t outWRe = __riscv_vmul_vx_i16m2(__riscv_vwcvt_x_x_v_i16m2(outRe, vl), C_16QAM, vl);
+        vint16m2_t outWIm = __riscv_vmul_vx_i16m2(__riscv_vwcvt_x_x_v_i16m2(outIm, vl), C_16QAM, vl);
 
-	    vint16m4x2_t outSeg = __riscv_vcreate_v_i16m4x2(outWRe, outWIm);
+	    vint16m2x2_t outSeg = __riscv_vcreate_v_i16m2x2(outWRe, outWIm);
 
-        __riscv_vsseg2e16_v_i16m4x2((int16_t*)pDst + i * 2, outSeg, vl);
+        __riscv_vsseg2e16_v_i16m2x2((int16_t*)pDst + i * 2, outSeg, vl);
 
         i += vl;
     }
@@ -326,45 +326,45 @@ void optQAM16_proxy(const uint8_t *pSrc, T16sc *pDst, uint32_t length)
 
 void optQAM64(const uint8_t *pSrc, T16sc *pDst, uint32_t length)
 {
-    size_t table_vl =  __riscv_vsetvl_e8m2(sizeof(mod64QAM_LUT) / sizeof(*mod64QAM_LUT));
+    size_t table_vl =  __riscv_vsetvl_e8m1(sizeof(mod64QAM_LUT) / sizeof(*mod64QAM_LUT));
 
-    vint8m2_t table = __riscv_vle8_v_i8m2(mod64QAM_LUT, table_vl);
+    vint8m1_t table = __riscv_vle8_v_i8m1(mod64QAM_LUT, table_vl);
 
     for (uint32_t i = 0; i < length;)
     {
-        size_t vl =  __riscv_vsetvl_e8m2(length - i);
+        size_t vl =  __riscv_vsetvl_e8m1(length - i);
 
-        vuint8m2_t src = __riscv_vle8_v_u8m2(pSrc + i, vl);
+        vuint8m1_t src = __riscv_vle8_v_u8m1(pSrc + i, vl);
 
-        vuint8m2_t b4 = __riscv_vand_vx_u8m2(src, 0x10, vl);
-        vuint8m2_t b3 = __riscv_vand_vx_u8m2(src, 0x08, vl);
-        vuint8m2_t b2 = __riscv_vand_vx_u8m2(src, 0x04, vl);
-        vuint8m2_t b1 = __riscv_vand_vx_u8m2(src, 0x02, vl);
+        vuint8m1_t b4 = __riscv_vand_vx_u8m1(src, 0x10, vl);
+        vuint8m1_t b3 = __riscv_vand_vx_u8m1(src, 0x08, vl);
+        vuint8m1_t b2 = __riscv_vand_vx_u8m1(src, 0x04, vl);
+        vuint8m1_t b1 = __riscv_vand_vx_u8m1(src, 0x02, vl);
         
-        vuint8m2_t swapped = __riscv_vor_vv_u8m2(
-            __riscv_vor_vv_u8m2(
-                __riscv_vsll_vx_u8m2(b1, 2, vl),
-                __riscv_vsrl_vx_u8m2(b2, 1, vl), vl),
-            __riscv_vor_vv_u8m2(
-                __riscv_vsll_vx_u8m2(b3, 1, vl),
-                __riscv_vsrl_vx_u8m2(b4, 2, vl), vl),
+        vuint8m1_t swapped = __riscv_vor_vv_u8m1(
+            __riscv_vor_vv_u8m1(
+                __riscv_vsll_vx_u8m1(b1, 2, vl),
+                __riscv_vsrl_vx_u8m1(b2, 1, vl), vl),
+            __riscv_vor_vv_u8m1(
+                __riscv_vsll_vx_u8m1(b3, 1, vl),
+                __riscv_vsrl_vx_u8m1(b4, 2, vl), vl),
             vl);
 
-        vuint8m2_t grouped = __riscv_vand_vx_u8m2(src, 0x21, vl);
-        grouped = __riscv_vor_vv_u8m2(grouped, swapped, vl);
+        vuint8m1_t grouped = __riscv_vand_vx_u8m1(src, 0x21, vl);
+        grouped = __riscv_vor_vv_u8m1(grouped, swapped, vl);
 
-        vuint8m2_t srcRe = __riscv_vsrl_vx_u8m2(__riscv_vand_vx_u8m2(grouped, 0x38, vl), 0x3, vl);
-        vuint8m2_t srcIm = __riscv_vand_vx_u8m2(grouped, 0x07, vl);
+        vuint8m1_t srcRe = __riscv_vsrl_vx_u8m1(__riscv_vand_vx_u8m1(grouped, 0x38, vl), 0x3, vl);
+        vuint8m1_t srcIm = __riscv_vand_vx_u8m1(grouped, 0x07, vl);
 
-        vint8m2_t outRe = __riscv_vrgather_vv_i8m2(table, srcRe, vl);
-        vint8m2_t outIm = __riscv_vrgather_vv_i8m2(table, srcIm, vl);
+        vint8m1_t outRe = __riscv_vrgather_vv_i8m1(table, srcRe, vl);
+        vint8m1_t outIm = __riscv_vrgather_vv_i8m1(table, srcIm, vl);
 
-        vint16m4_t outWRe = __riscv_vmul_vx_i16m4(__riscv_vwcvt_x_x_v_i16m4(outRe, vl), C_64QAM, vl);
-        vint16m4_t outWIm = __riscv_vmul_vx_i16m4(__riscv_vwcvt_x_x_v_i16m4(outIm, vl), C_64QAM, vl);
+        vint16m2_t outWRe = __riscv_vmul_vx_i16m2(__riscv_vwcvt_x_x_v_i16m2(outRe, vl), C_64QAM, vl);
+        vint16m2_t outWIm = __riscv_vmul_vx_i16m2(__riscv_vwcvt_x_x_v_i16m2(outIm, vl), C_64QAM, vl);
 
-	    vint16m4x2_t outSeg = __riscv_vcreate_v_i16m4x2(outWRe, outWIm);
+	    vint16m2x2_t outSeg = __riscv_vcreate_v_i16m2x2(outWRe, outWIm);
 
-        __riscv_vsseg2e16_v_i16m4x2((int16_t*)pDst + i * 2, outSeg, vl);
+        __riscv_vsseg2e16_v_i16m2x2((int16_t*)pDst + i * 2, outSeg, vl);
 
         i += vl;
     }
@@ -372,30 +372,30 @@ void optQAM64(const uint8_t *pSrc, T16sc *pDst, uint32_t length)
 
 void optQAM64_proxy(const uint8_t *pSrc, T16sc *pDst, uint32_t length)
 {
-    size_t table_vl =  __riscv_vsetvl_e8m2(sizeof(mod64QAM_LUT) / sizeof(*mod64QAM_LUT));
+    size_t table_vl =  __riscv_vsetvl_e8m1(sizeof(mod64QAM_LUT) / sizeof(*mod64QAM_LUT));
 
-    vint8m2_t table = __riscv_vle8_v_i8m2(mod64QAM_LUT, table_vl);
+    vint8m1_t table = __riscv_vle8_v_i8m1(mod64QAM_LUT, table_vl);
 
     for (uint32_t i = 0; i < length;)
     {
-        size_t vl =  __riscv_vsetvl_e8m2(length - i);
+        size_t vl =  __riscv_vsetvl_e8m1(length - i);
 
-        vuint8m2_t src = __riscv_vle8_v_u8m2(pSrc + i, vl);
+        vuint8m1_t src = __riscv_vle8_v_u8m1(pSrc + i, vl);
 
-        vuint8m2_t grouped = __riscv_vand_vx_u8m2(src, 0x9, vl); // assume it is vector unzip
+        vuint8m1_t grouped = __riscv_vand_vx_u8m1(src, 0x9, vl); // assume it is vector unzip
 
-        vuint8m2_t srcRe = __riscv_vsrl_vx_u8m2(__riscv_vand_vx_u8m2(grouped, 0x38, vl), 0x3, vl);
-        vuint8m2_t srcIm = __riscv_vand_vx_u8m2(grouped, 0x07, vl);
+        vuint8m1_t srcRe = __riscv_vsrl_vx_u8m1(__riscv_vand_vx_u8m2(grouped, 0x38, vl), 0x3, vl);
+        vuint8m1_t srcIm = __riscv_vand_vx_u8m1(grouped, 0x07, vl);
 
-        vint8m2_t outRe = __riscv_vrgather_vv_i8m2(table, srcRe, vl);
-        vint8m2_t outIm = __riscv_vrgather_vv_i8m2(table, srcIm, vl);
+        vint8m1_t outRe = __riscv_vrgather_vv_i8m1(table, srcRe, vl);
+        vint8m1_t outIm = __riscv_vrgather_vv_i8m1(table, srcIm, vl);
 
-        vint16m4_t outWRe = __riscv_vmul_vx_i16m4(__riscv_vwcvt_x_x_v_i16m4(outRe, vl), C_256QAM, vl);
-        vint16m4_t outWIm = __riscv_vmul_vx_i16m4(__riscv_vwcvt_x_x_v_i16m4(outIm, vl), C_256QAM, vl);
+        vint16m2_t outWRe = __riscv_vmul_vx_i16m2(__riscv_vwcvt_x_x_v_i16m2(outRe, vl), C_256QAM, vl);
+        vint16m2_t outWIm = __riscv_vmul_vx_i16m2(__riscv_vwcvt_x_x_v_i16m2(outIm, vl), C_256QAM, vl);
 
-	    vint16m4x2_t outSeg = __riscv_vcreate_v_i16m4x2(outWRe, outWIm);
+	    vint16m2x2_t outSeg = __riscv_vcreate_v_i16m2x2(outWRe, outWIm);
 
-        __riscv_vsseg2e16_v_i16m4x2((int16_t*)pDst + i * 2, outSeg, vl);
+        __riscv_vsseg2e16_v_i16m2x2((int16_t*)pDst + i * 2, outSeg, vl);
 
         i += vl;
     }
@@ -403,53 +403,53 @@ void optQAM64_proxy(const uint8_t *pSrc, T16sc *pDst, uint32_t length)
 
 void optQAM256(const uint8_t *pSrc, T16sc *pDst, uint32_t length)
 {
-    size_t table_vl =  __riscv_vsetvl_e8m2(sizeof(mod256QAM_LUT) / sizeof(*mod256QAM_LUT));
+    size_t table_vl =  __riscv_vsetvl_e8m1(sizeof(mod256QAM_LUT) / sizeof(*mod256QAM_LUT));
 
-    vint8m2_t table = __riscv_vle8_v_i8m2(mod256QAM_LUT, table_vl);
+    vint8m1_t table = __riscv_vle8_v_i8m1(mod256QAM_LUT, table_vl);
 
     for (uint32_t i = 0; i < length;)
     {
         size_t vl =  __riscv_vsetvl_e8m2(length - i);
 
-        vuint8m2_t src = __riscv_vle8_v_u8m2(pSrc + i, vl);
+        vuint8m1_t src = __riscv_vle8_v_u8m2(pSrc + i, vl);
 
-        vuint8m2_t b6 = __riscv_vand_vx_u8m2(src, 0x40, vl);
-        vuint8m2_t b5 = __riscv_vand_vx_u8m2(src, 0x20, vl);
-        vuint8m2_t b4 = __riscv_vand_vx_u8m2(src, 0x10, vl);
-        vuint8m2_t b3 = __riscv_vand_vx_u8m2(src, 0x08, vl);
-        vuint8m2_t b2 = __riscv_vand_vx_u8m2(src, 0x04, vl);
-        vuint8m2_t b1 = __riscv_vand_vx_u8m2(src, 0x02, vl);
+        vuint8m1_t b6 = __riscv_vand_vx_u8m1(src, 0x40, vl);
+        vuint8m1_t b5 = __riscv_vand_vx_u8m1(src, 0x20, vl);
+        vuint8m1_t b4 = __riscv_vand_vx_u8m1(src, 0x10, vl);
+        vuint8m1_t b3 = __riscv_vand_vx_u8m1(src, 0x08, vl);
+        vuint8m1_t b2 = __riscv_vand_vx_u8m1(src, 0x04, vl);
+        vuint8m1_t b1 = __riscv_vand_vx_u8m1(src, 0x02, vl);
         
-        vuint8m2_t swapped = __riscv_vor_vv_u8m2(
-            __riscv_vor_vv_u8m2(
-                __riscv_vsll_vx_u8m2(b1, 3, vl),
-                __riscv_vsrl_vx_u8m2(b2, 1, vl), vl),
-            __riscv_vor_vv_u8m2(
-                __riscv_vsll_vx_u8m2(b3, 2, vl),
-                __riscv_vsrl_vx_u8m2(b4, 2, vl), vl),
+        vuint8m1_t swapped = __riscv_vor_vv_u8m1(
+            __riscv_vor_vv_u8m1(
+                __riscv_vsll_vx_u8m1(b1, 3, vl),
+                __riscv_vsrl_vx_u8m1(b2, 1, vl), vl),
+            __riscv_vor_vv_u8m1(
+                __riscv_vsll_vx_u8m1(b3, 2, vl),
+                __riscv_vsrl_vx_u8m1(b4, 2, vl), vl),
             vl);
 
-        swapped = __riscv_vor_vv_u8m2(swapped, 
-            __riscv_vor_vv_u8m2(
-                __riscv_vsll_vx_u8m2(b5, 1, vl),
-                __riscv_vsrl_vx_u8m2(b6, 3, vl), vl), 
+        swapped = __riscv_vor_vv_u8m1(swapped, 
+            __riscv_vor_vv_u8m1(
+                __riscv_vsll_vx_u8m1(b5, 1, vl),
+                __riscv_vsrl_vx_u8m1(b6, 3, vl), vl), 
             vl);
 
-        vuint8m2_t grouped = __riscv_vand_vx_u8m2(src, 0x81, vl);
+        vuint8m1_t grouped = __riscv_vand_vx_u8m1(src, 0x81, vl);
         grouped = __riscv_vor_vv_u8m2(grouped, swapped, vl);
 
-        vuint8m2_t srcRe = __riscv_vsrl_vx_u8m2(__riscv_vand_vx_u8m2(grouped, 0xF0, vl), 0x4, vl);
-        vuint8m2_t srcIm = __riscv_vand_vx_u8m2(grouped, 0x0F, vl);
+        vuint8m1_t srcRe = __riscv_vsrl_vx_u8m1(__riscv_vand_vx_u8m1(grouped, 0xF0, vl), 0x4, vl);
+        vuint8m1_t srcIm = __riscv_vand_vx_u8m1(grouped, 0x0F, vl);
 
-        vint8m2_t outRe = __riscv_vrgather_vv_i8m2(table, srcRe, vl);
-        vint8m2_t outIm = __riscv_vrgather_vv_i8m2(table, srcIm, vl);
+        vint8m1_t outRe = __riscv_vrgather_vv_i8m1(table, srcRe, vl);
+        vint8m1_t outIm = __riscv_vrgather_vv_i8m1(table, srcIm, vl);
 
-        vint16m4_t outWRe = __riscv_vmul_vx_i16m4(__riscv_vwcvt_x_x_v_i16m4(outRe, vl), C_256QAM, vl);
-        vint16m4_t outWIm = __riscv_vmul_vx_i16m4(__riscv_vwcvt_x_x_v_i16m4(outIm, vl), C_256QAM, vl);
+        vint16m2_t outWRe = __riscv_vmul_vx_i16m2(__riscv_vwcvt_x_x_v_i16m2(outRe, vl), C_256QAM, vl);
+        vint16m2_t outWIm = __riscv_vmul_vx_i16m2(__riscv_vwcvt_x_x_v_i16m2(outIm, vl), C_256QAM, vl);
 
-	    vint16m4x2_t outSeg = __riscv_vcreate_v_i16m4x2(outWRe, outWIm);
+	    vint16m2x2_t outSeg = __riscv_vcreate_v_i16m2x2(outWRe, outWIm);
 
-        __riscv_vsseg2e16_v_i16m4x2((int16_t*)pDst + i * 2, outSeg, vl);
+        __riscv_vsseg2e16_v_i16m2x2((int16_t*)pDst + i * 2, outSeg, vl);
 
         i += vl;
     }
@@ -457,28 +457,28 @@ void optQAM256(const uint8_t *pSrc, T16sc *pDst, uint32_t length)
 
 void optQAM256_proxy(const uint8_t *pSrc, T16sc *pDst, uint32_t length)
 {
-    size_t table_vl =  __riscv_vsetvl_e8m2(sizeof(mod256QAM_LUT) / sizeof(*mod256QAM_LUT));
+    size_t table_vl =  __riscv_vsetvl_e8m1(sizeof(mod256QAM_LUT) / sizeof(*mod256QAM_LUT));
 
-    vint8m2_t table = __riscv_vle8_v_i8m2(mod256QAM_LUT, table_vl);
+    vint8m1_t table = __riscv_vle8_v_i8m1(mod256QAM_LUT, table_vl);
 
     for (uint32_t i = 0; i < length;)
     {
-        size_t vl =  __riscv_vsetvl_e8m2(length - i);
+        size_t vl =  __riscv_vsetvl_e8m1(length - i);
 
-        vuint8m2_t src = __riscv_vle8_v_u8m2(pSrc + i, vl);
+        vuint8m1_t src = __riscv_vle8_v_u8m1(pSrc + i, vl);
 
-        vuint8m2_t grouped = __riscv_vand_vx_u8m2(src, 0x9, vl); // assume it is vector unzip
+        vuint8m1_t grouped = __riscv_vand_vx_u8m1(src, 0x9, vl); // assume it is vector unzip
 
-        vuint8m2_t srcRe = __riscv_vsrl_vx_u8m2(__riscv_vand_vx_u8m2(grouped, 0xF0, vl), 0x4, vl);
-        vuint8m2_t srcIm = __riscv_vand_vx_u8m2(grouped, 0x0F, vl);
+        vuint8m1_t srcRe = __riscv_vsrl_vx_u8m1(__riscv_vand_vx_u8m1(grouped, 0xF0, vl), 0x4, vl);
+        vuint8m1_t srcIm = __riscv_vand_vx_u8m1(grouped, 0x0F, vl);
 
-        vint8m2_t outRe = __riscv_vrgather_vv_i8m2(table, srcRe, vl);
-        vint8m2_t outIm = __riscv_vrgather_vv_i8m2(table, srcIm, vl);
+        vint8m1_t outRe = __riscv_vrgather_vv_i8m1(table, srcRe, vl);
+        vint8m1_t outIm = __riscv_vrgather_vv_i8m1(table, srcIm, vl);
 
-        vint16m4_t outWRe = __riscv_vmul_vx_i16m4(__riscv_vwcvt_x_x_v_i16m4(outRe, vl), C_256QAM, vl);
-        vint16m4_t outWIm = __riscv_vmul_vx_i16m4(__riscv_vwcvt_x_x_v_i16m4(outIm, vl), C_256QAM, vl);
+        vint16m2_t outWRe = __riscv_vmul_vx_i16m2(__riscv_vwcvt_x_x_v_i16m2(outRe, vl), C_256QAM, vl);
+        vint16m2_t outWIm = __riscv_vmul_vx_i16m2(__riscv_vwcvt_x_x_v_i16m2(outIm, vl), C_256QAM, vl);
 
-	    vint16m4x2_t outSeg = __riscv_vcreate_v_i16m4x2(outWRe, outWIm);
+	    vint16m2x2_t outSeg = __riscv_vcreate_v_i16m2x2(outWRe, outWIm);
 
         __riscv_vsseg2e16_v_i16m4x2((int16_t*)pDst + i * 2, outSeg, vl);
 
